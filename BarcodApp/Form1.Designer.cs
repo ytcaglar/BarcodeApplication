b68703ce4fa1d3f199d9b5bb1674617d5fba8e6a
tolun <tolun@DESKTOP-5T4TP88>
2023-05-20 04:59:28 +0300

    Proje dosyası ekle.

---
+++ BarcodApp/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace BarcodApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.barcodPic = new System.Windows.Forms.PictureBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.barcodPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Barcode No : ";
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(153, 32);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(266, 30);
+            this.txtBarcode.TabIndex = 1;
+            // 
+            // barcodPic
+            // 
+            this.barcodPic.BackColor = System.Drawing.Color.LightGray;
+            this.barcodPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.barcodPic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.barcodPic.Location = new System.Drawing.Point(19, 80);
+            this.barcodPic.Name = "barcodPic";
+            this.barcodPic.Size = new System.Drawing.Size(400, 350);
+            this.barcodPic.TabIndex = 2;
+            this.barcodPic.TabStop = false;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(16, 436);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(131, 40);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(153, 436);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(127, 40);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(286, 436);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(133, 40);
+            this.btnRead.TabIndex = 5;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 501);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.barcodPic);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Barcode App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.barcodPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBarcode;
+        private System.Windows.Forms.PictureBox barcodPic;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
---
+++ BarcodApp/Form1.cs
@@ -0,0 +1,87 @@
+﻿using BarcodeLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace BarcodApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            BarcodeGen();
+        }
+
+        private void BarcodeGen()
+        {
+            Barcode barcode = new Barcode();
+            Color backColor = Color.Transparent;
+            Color foreColor = Color.Black;
+            Image image = barcode.Encode(TYPE.CODE128, txtBarcode.Text, foreColor, backColor, 400, 350);
+            barcodPic.Image = image;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog saveFolderDialog = new FolderBrowserDialog() { Description = "Select path."})
+            {
+                if (saveFolderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string path = saveFolderDialog.SelectedPath;
+                    barcodPic.Image.Save(path + $"/{txtBarcode.Text}.png");
+                    MessageBox.Show("Barcode saved.");
+                    barcodPic.Image = null;
+                    txtBarcode.Text = null;
+                }
+            }
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string path = "";
+            using (OpenFileDialog FolderDialog = new OpenFileDialog() { Filter = "PNG|*.png"})
+            {
+                if (FolderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = FolderDialog.FileName;
+                }
+            }
+            if (path != "")
+            {
+                BarcodeReader barcodeReader = new BarcodeReader();
+                Bitmap bmp = new Bitmap(path);
+                var decodeBarcode = barcodeReader.Decode(bmp);
+
+                if (decodeBarcode != null)
+                {
+                    txtBarcode.Text = decodeBarcode.ToString();
+                    BarcodeGen();
+                    MessageBox.Show("Barcode Readed!");
+                }
+                else
+                {
+                    MessageBox.Show("You made the wrong choice. Please, select a barcode!");
+                }
+            }
+            
+        }
+    }
+}
